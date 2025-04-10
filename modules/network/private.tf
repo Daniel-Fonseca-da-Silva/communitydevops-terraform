ffb3b4e595f95525b6f5e6123f751227a4836552
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-04-10 20:07:37 +0100

    fix: Name zone

--- modules/network/private.tf
+++ modules/network/private.tf
@@ -1,10 +1,10 @@
 resource "aws_subnet" "eks_subnet_private_1a" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
-  availability_zone = "${data.aws_region.current.name}a"
+  availability_zone = "us-east-a"
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                              = "${var.project_name}-priv-subnet-1a"
       "kubernetes.io/role/internal-elb" = "1"
@@ -15,10 +15,10 @@ resource "aws_subnet" "eks_subnet_private_1a" {
 resource "aws_subnet" "eks_subnet_private_1b" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
-  availability_zone = "${data.aws_region.current.name}b"
+  availability_zone = "us-east-b"
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                              = "${var.project_name}-priv-subnet-1b"
       "kubernetes.io/role/internal-elb" = "1"
